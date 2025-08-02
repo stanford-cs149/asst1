26189708352b67959e0b1f3bec42fdd69926de2a
Haiyun Xu <xuhaiyunhenry@gmail.com>
2025-08-01 20:59:00 -0400

    completed prog4;

--- prog4_sqrt/main.cpp
+++ prog4_sqrt/main.cpp
@@ -10,6 +10,8 @@ using namespace ispc;
 
 extern void sqrtSerial(int N, float startGuess, float* values, float* output);
 
+extern void sqrtAvx2(int N, float initialGuess, float values[], float output[]);
+
 static void verifyResult(int N, float* result, float* gold) {
     for (int i=0; i<N; i++) {
         if (fabs(result[i] - gold[i]) > 1e-4) {
@@ -28,18 +30,15 @@ int main() {
     float* gold = new float[N];
 
     for (unsigned int i=0; i<N; i++)
-    {
-        // TODO: CS149 students.  Attempt to change the values in the
-        // array here to meet the instructions in the handout: we want
-        // to you generate best and worse-case speedups
-        
-        // starter code populates array with random input values
+    {   
         values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
     }
 
     // generate a gold version to check results
     for (unsigned int i=0; i<N; i++)
         gold[i] = sqrt(values[i]);
+
+
 
     //
     // And run the serial implementation 3 times, again reporting the
@@ -57,6 +56,33 @@ int main() {
 
     verifyResult(N, output, gold);
 
+
+
+    // Clear out the buffer
+    for (unsigned int i = 0; i < N; ++i)
+        output[i] = 0;
+
+    //
+    // Run the AVX2 implementation
+    //
+    double minAvx2 = 1e30;
+    for (int i = 0; i < 3; ++i) {
+        double startTime = CycleTimer::currentSeconds();
+        sqrtAvx2(N, initialGuess, values, output);
+        double endTime = CycleTimer::currentSeconds();
+        minAvx2 = std::min(minAvx2, endTime - startTime);
+    }
+    
+    printf("[sqrt avx2]:\t\t[%.3f] ms\n", minAvx2 * 1000);
+
+    verifyResult(N, output, gold);
+
+
+
+    // Clear out the buffer
+    for (unsigned int i = 0; i < N; ++i)
+        output[i] = 0;
+
     //
     // Compute the image using the ispc implementation; report the minimum
     // time of three runs.
@@ -72,6 +98,8 @@ int main() {
     printf("[sqrt ispc]:\t\t[%.3f] ms\n", minISPC * 1000);
 
     verifyResult(N, output, gold);
+
+
 
     // Clear out the buffer
     for (unsigned int i = 0; i < N; ++i)
@@ -92,6 +120,9 @@ int main() {
 
     verifyResult(N, output, gold);
 
+
+
+    printf("\t\t\t\t(%.2fx speedup from AVX2)\n", minSerial/minAvx2);
     printf("\t\t\t\t(%.2fx speedup from ISPC)\n", minSerial/minISPC);
     printf("\t\t\t\t(%.2fx speedup from task ISPC)\n", minSerial/minTaskISPC);
 
